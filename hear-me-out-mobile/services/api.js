399bb26e32cd1784fc126fecb633c8860b2631b2
friedballa9 <friedballs9@gmail.com>
2026-05-10 14:10:03 +0800

    fix: update mobile API URL

--- hear-me-out-mobile/services/api.js
+++ hear-me-out-mobile/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const BASE_URL = "https://hear-me-out-backend.up.railway.app/api";
+const BASE_URL = "https://hear-me-out-backend-production.up.railway.app/api";
 
 const API = axios.create({ baseURL: BASE_URL });
 
